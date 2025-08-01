1850f19ac54b7c0a8e52568fea2ddddad0b9c783
Ben Alterman <alterman.ben@gmail.com>
2025-08-01 02:27:24 +0000

    This is the icon for NASA ADS. Update the SVG icon with it.

--- src/components/icons/nasa-ads-icon.tsx
+++ src/components/icons/nasa-ads-icon.tsx
@@ -2,8 +2,13 @@ import { SVGProps } from "react";
 
 export function NasaAdsIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg {...props} viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path fill="currentColor" d="M128 25.6c-56.32 0-102.4 46.08-102.4 102.4s46.08 102.4 102.4 102.4 102.4-46.08 102.4-102.4S184.32 25.6 128 25.6zm-42.66 50.38h19.53l33.35 66.86h.51l33.35-66.86h19.53v105.1h-17.02v-73.8h-.51l-34.42 69.17h-4.04L86.4 107.28h-.51v73.8H68.87V75.98h16.47z"/>
+    <svg
+      {...props}
+      viewBox="0 0 512 512"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M256,512c141.385,0,256-114.615,256-256S397.385,0,256,0,0,114.615,0,256,114.615,512,256,512ZM139.46,168.146h29.475L256,312.311l87.065-144.165h29.475L256,354.34Z" />
     </svg>
   );
 }
