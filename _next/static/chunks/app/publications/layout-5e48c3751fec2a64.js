0e637f75d62848e9eddd8419655848c358b9a6a6
blalterman <blalterman@users.noreply.github.com>
2025-08-06 08:11:28 +0000

    deploy: f0f6eae94786cbbe6fdf483aa181b51d82a7a160

---
+++ _next/static/chunks/app/publications/layout-5e48c3751fec2a64.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[373,499,784],{2884:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,6874,23)),Promise.resolve().then(n.t.bind(n,3063,23)),Promise.resolve().then(n.bind(n,1923)),Promise.resolve().then(n.bind(n,3936))}},e=>{var s=s=>e(e.s=s);e.O(0,[63,3,874,577,454,441,684,358],()=>s(2884)),_N_E=e.O()}]);
