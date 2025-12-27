1252005f19a71bb3fd53d2a467931daf6c4bd9ac
blalterman <blalterman@users.noreply.github.com>
2025-12-27 02:01:25 +0000

    deploy: e8f28c5e2b1fc8c9b1b969735f22cd2543772c86

---
+++ _next/static/chunks/app/ben/layout-8fbbbff24ccb4bc2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[373,499,755,784],{2884:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,1356,23)),Promise.resolve().then(n.bind(n,4392)),Promise.resolve().then(n.bind(n,2870))}},e=>{e.O(0,[619,898,356,595,139,441,255,358],()=>e(e.s=2884)),_N_E=e.O()}]);
